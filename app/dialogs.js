6ee2fcb0da4468a434410eff86ebf353f8da2236
brrd <contact@brrd.fr>
2016-04-15 22:23:00 +0200

    Fix "about" dialog crashing the app when called from the main process
    Because dialog.showMessageBox throws an error when passing undefined as its first argument.

--- app/dialogs.js
+++ app/dialogs.js
@@ -27,13 +27,18 @@ var appDialogs = {
 
     about: function (win) {
         win = getWindow(win);
-        var image = NativeImage.createFromPath(constants.path.icon);
-        dialog.showMessageBox(win, {
-            title: "About",
-            message: "ABRICOTINE - MARKDOWN EDITOR (v. " + constants.appVersion + ")\n\nCopyright (c) 2015 Thomas Brouard\n\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.",
-            buttons: ['OK'],
-            icon: image
-        });
+        var image = NativeImage.createFromPath(constants.path.icon),
+            options = {
+                title: "About",
+                message: "ABRICOTINE - MARKDOWN EDITOR (v. " + constants.appVersion + ")\n\nCopyright (c) 2015 Thomas Brouard\n\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.",
+                buttons: ['OK'],
+                icon: image
+            };
+        if (win) {
+            dialog.showMessageBox(win, options);
+        } else {
+            dialog.showMessageBox(options);
+        }
     },
 
     askClose : function (abrDoc, closeFunc, win) {
