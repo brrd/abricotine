brrd/abricotine

commit bba393c3751b4b29eefdd8e94c14ccdbcd32611c

Moving dialogs to main process

app/dialogs.js
@@ -4,11 +4,10 @@
 *   Licensed under GNU-GPLv3 <http://www.gnu.org/licenses/gpl.html>
 */
 
-var remote = require("remote"),
-    BrowserWindow = remote.require("browser-window"),
-    constants = remote.require("./constants.js"),
-    dialog = remote.require("dialog"),
-    NativeImage = remote.require("native-image"),
+var BrowserWindow = require("browser-window"),
+    constants = require("./constants.js"),
+    dialog = require("dialog"),
+    NativeImage = require("native-image"),
     parsePath = require("parse-filepath");
 
 // Returns the most "logical" window object (it is quite useless actually)
@@ -19,8 +18,6 @@ function getWindow (win) {
         return win;
     } else if (win && typeof win.browserWindow !== "undefined") {
         return win.browserWindow;
-    } else if (typeof remote !== "undefined") {
-        return remote.getCurrentWindow();
     } else {
         return BrowserWindow.getFocusedWindow();
     }

app/renderer/abr-document.js
@@ -9,7 +9,7 @@ var remote = require("remote"),
     cmInit = require.main.require("./cm-init.js"),
     commands = require.main.require("./commands.js"),
     constants = remote.require("./constants.js"),
-    dialogs = require.main.require("./dialogs.js"),
+    dialogs = remote.require("./dialogs.js"),
     imageImport = require.main.require("./image-import.js"),
     IpcClient = require.main.require("./ipc-client.js"),
     exportHtml = require.main.require("./export-html.js"),

app/renderer/main.js
@@ -4,8 +4,9 @@
 *   Licensed under GNU-GPLv3 <http://www.gnu.org/licenses/gpl.html>
 */
 
-var AbrDocument = require.main.require("./abr-document.js"),
-    dialogs = require.main.require("./dialogs.js");
+var remote = require("remote"),
+    AbrDocument = require.main.require("./abr-document.js"),
+    dialogs = remote.require("./dialogs.js");
 
 $( function () {
     var abrDoc = new AbrDocument();

app/renderer/export-html.js
@@ -6,7 +6,7 @@
 
 var remote = require("remote"),
     constants = remote.require("./constants"),
-    dialogs = require.main.require("./dialogs.js"),
+    dialogs = remote.require("./dialogs.js"),
     files = remote.require("./files.js"),
     md2html = require.main.require("./md2html.js"),
     parsePath = require("parse-filepath"),

app/renderer/image-import.js
@@ -5,7 +5,7 @@
 */
 
 var remote = require("remote"),
-    dialogs = require.main.require("./dialogs.js"),
+    dialogs = remote.require("./dialogs.js"),
     files = remote.require("./files.js"),
     isUrl = require("is-url"),
     pathModule = require("path"),

commit 271908657d8fbff30e52af7d5f2ff193ada31361

Fix askClose dialog not working
Issue introduced in commit bba393c3751b4b29eefdd8e94c14ccdbcd32611c

app/dialogs.js
@@ -41,13 +41,11 @@ var appDialogs = {
         }
     },
 
-    askClose : function (abrDoc, closeFunc, win) {
-        var path = abrDoc.path;
+    askClose: function (path, saveFunc, closeFunc, win) {
         if (!path) {
             path = 'New document';
         }
         win = getWindow(win);
-        closeFunc = closeFunc || win.destroy; // win.close() would trigger the 'onbeforeunload' event again
         var filename = parsePath(path).basename || path,
             userChoice = dialog.showMessageBox(win, {
                 title: 'Unsaved document',
@@ -56,7 +54,7 @@ var appDialogs = {
             });
         switch (userChoice) {
             case 1:
-                abrDoc.save(null, closeFunc);
+                saveFunc(closeFunc);
                 break;
             case 2:
                 closeFunc();

app/renderer/abr-document.js
@@ -134,6 +134,21 @@ function AbrDocument () {
             }
         };
         document.getElementById("editor").addEventListener("mousewheel", MouseWheelHandler, false);
+
+        // Close event
+        window.onbeforeunload = function(e) {
+            if (!that.isClean()) {
+                e.returnValue = false;
+                var win = remote.getCurrentWindow(),
+                    saveFunc = function (callback) {
+                        that.save(null, callback);
+                    },
+                    closeFunc = function () {
+                        win.destroy();
+                    };
+                dialogs.askClose(that.path, saveFunc, closeFunc);
+            }
+        };
     });
 }
 
@@ -153,11 +168,14 @@ AbrDocument.prototype = {
 
     close: function (force) {
         var that = this,
+            saveFunc = function (callback) {
+                that.save(null, callback);
+            },
             closeFunc = function () {
                 that.clear();
             };
         if (!force && !this.isClean()) {
-            dialogs.askClose(this, closeFunc);
+            dialogs.askClose(that.path, saveFunc, closeFunc);
         } else {
             closeFunc();
         }

app/renderer/main.js
@@ -4,20 +4,9 @@
 *   Licensed under GNU-GPLv3 <http://www.gnu.org/licenses/gpl.html>
 */
 
-var remote = require("remote"),
-    AbrDocument = require.main.require("./abr-document.js"),
-    dialogs = remote.require("./dialogs.js");
+var AbrDocument = require.main.require("./abr-document.js");
 
 $( function () {
     var abrDoc = new AbrDocument();
-
-    // Close event
-    window.onbeforeunload = function(e) {
-        if (!abrDoc.isClean()) {
-            e.returnValue = false;
-            dialogs.askClose(abrDoc);
-        }
-    };
-
     window.abrDoc = abrDoc;
 });