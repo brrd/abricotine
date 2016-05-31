1c3bb08ff8758a6c56cf9dde4419ea16b7c8741c
brrd <contact@brrd.fr>
2016-05-31 14:32:21 +0200

    Remove abr-pane-worker.js wrapping function

--- app/renderer/abr-pane-worker.js
+++ app/renderer/abr-pane-worker.js
@@ -4,67 +4,65 @@
 *   Licensed under GNU-GPLv3 <http://www.gnu.org/licenses/gpl.html>
 */
 
-(function () {
-    // Find headers in text and return ToC
-    function getToc (text) {
-        var toc = [],
-            lines = text.split("\n");
-        lines.forEach( function (line, index) {
-            // # headers
-            var headerMatch = line.match(/^(#+)(.*)#*$/);
-            if (headerMatch) {
-                toc.push({
-                    content: headerMatch[2].trim(),
-                    level: headerMatch[1].length,
-                    line: index
-                });
-            }
-            // Underlined headers
-            var underlineMatch = line.match(/^\s*(=|-)+$/);
-            if (
-                underlineMatch &&
-                // not first line
-                index !== 0 &&
-                // header isnt already registered
-                !(toc.length > 0 && toc[toc.length - 1].line === index - 1)
-            ) {
-                toc.push({
-                    content: (lines[index - 1] || "").trim(),
-                    level: underlineMatch[1] === "=" ? 1 : 2,
-                    line: index - 1
-                });
-            }
-        });
-        return toc;
-    }
-
-    // Return index of the current active header
-    function getActiveHeaderIndex (toc, cursorLine) {
-        var res;
-        toc.forEach( function (header, index) {
-            if (cursorLine >= header.line && header.content) {
-                res = index;
-            } else {
-                return;
-            }
-        });
-        return res;
-    }
-
-    // Keep a track of toc
-    var toc;
-
-    // Listen to AbrPane
-    process.on("message", function(msg) {
-        var answer = {};
-        // Update toc if text provided ("changes" event)
-        if (msg.text != null) {
-            answer.toc = toc = getToc(msg.text);
+// Find headers in text and return ToC
+function getToc (text) {
+    var toc = [],
+        lines = text.split("\n");
+    lines.forEach( function (line, index) {
+        // # headers
+        var headerMatch = line.match(/^(#+)(.*)#*$/);
+        if (headerMatch) {
+            toc.push({
+                content: headerMatch[2].trim(),
+                level: headerMatch[1].length,
+                line: index
+            });
         }
-        // Don't send anything if toc empty
-        if (!toc || toc.length === 0) return;
-        // Update active header ("changes" + "cursorActivity" events)
-        answer.activeHeaderIndex = getActiveHeaderIndex(toc, msg.cursorLine);
-        process.send(answer);
-    }, false);
-})();
+        // Underlined headers
+        var underlineMatch = line.match(/^\s*(=|-)+$/);
+        if (
+            underlineMatch &&
+            // not first line
+            index !== 0 &&
+            // header isnt already registered
+            !(toc.length > 0 && toc[toc.length - 1].line === index - 1)
+        ) {
+            toc.push({
+                content: (lines[index - 1] || "").trim(),
+                level: underlineMatch[1] === "=" ? 1 : 2,
+                line: index - 1
+            });
+        }
+    });
+    return toc;
+}
+
+// Return index of the current active header
+function getActiveHeaderIndex (toc, cursorLine) {
+    var res;
+    toc.forEach( function (header, index) {
+        if (cursorLine >= header.line && header.content) {
+            res = index;
+        } else {
+            return;
+        }
+    });
+    return res;
+}
+
+// Keep a track of toc
+var toc;
+
+// Listen to AbrPane
+process.on("message", function(msg) {
+    var answer = {};
+    // Update toc if text provided ("changes" event)
+    if (msg.text != null) {
+        answer.toc = toc = getToc(msg.text);
+    }
+    // Don't send anything if toc empty
+    if (!toc || toc.length === 0) return;
+    // Update active header ("changes" + "cursorActivity" events)
+    answer.activeHeaderIndex = getActiveHeaderIndex(toc, msg.cursorLine);
+    process.send(answer);
+}, false);
