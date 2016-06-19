44d62a58292c770cafed1bc9bde2330477d32292
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-06-19 15:12:34 +0200

    Handle invalid or missing i18n data file

--- app/localize.js
+++ app/localize.js
@@ -6,15 +6,18 @@ var constants = require("./constants.js"),
 var translations = {};
 
 module.exports.load = function (language) {
-    var languageFile = paths.join(constants.path.languages, language + ".json");
+    var languageFile = paths.join(constants.path.languages, language + ".json"),
+        fileContent;
     if (files.fileExists(languageFile)) {
         // TODO find approaching locale when exact language file does not exist (e.g. load "fr.json" when "fr_BE.json" is asked)
-        // TODO handle parsing errors (e.g. file is not valid JSON)
-        var fileContent = fs.readFileSync(languageFile, 'utf8');
-        translations = JSON.parse(fileContent);
-    }
-    else {
-        console.warn("Missing translation for " + language);
+        try {
+            fileContent = fs.readFileSync(languageFile, 'utf8');
+            translations = JSON.parse(fileContent);
+        } catch (err) {
+            console.warn("Error while loading localization data (from file '" + languageFile + "'): " + err);
+        }
+    } else {
+        console.warn("Error while loading localization data (from file '" + languageFile + "'): file does not exist");
     }
 };
 
