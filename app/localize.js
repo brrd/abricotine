6c937b068786ef9015c4db0e446800ab936473de
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-06-19 10:23:39 +0200

    Added: TODOs

--- app/localize.js
+++ app/localize.js
@@ -8,6 +8,8 @@ var translations = {};
 module.exports.load = function (language) {
     var languageFile = paths.join(constants.path.languages, language + ".json");
     if (files.fileExists(languageFile)) {
+        // TODO find approaching locale when exact language file does not exist (e.g. load "fr.json" when "fr_BE.json" is asked)
+        // TODO handle parsing errors (e.g. file is not valid JSON)
         var fileContent = fs.readFileSync(languageFile, 'utf8');
         translations = JSON.parse(fileContent);
     }
