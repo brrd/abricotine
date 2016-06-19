48aa68940d4181f22f8ee0b335ba5cb13a1b24f0
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-06-19 19:17:00 +0200

    Added: License

--- app/localize.js
+++ app/localize.js
@@ -1,3 +1,9 @@
+/*
+*   Abricotine - Markdown Editor
+*   Copyright (c) 2015 Thomas Brouard
+*   Licensed under GNU-GPLv3 <http://www.gnu.org/licenses/gpl.html>
+*/
+
 var constants = require("./constants.js"),
     files = require.main.require("./files.js"),
     paths = require("path"),
