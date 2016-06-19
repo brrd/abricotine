0080228ade2326aec9d54f62add9fabe26d1f747
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-06-19 10:19:49 +0200

    Added: languages dir

--- app/constants.js
+++ app/constants.js
@@ -21,6 +21,7 @@ module.exports = {
         icon: pathModule.join(appPath, "/icons/abricotine.png"),
         defaultDir: isAsar ? appPath + ".unpacked/default" :  appPath + "/default",
         dictionaries: pathModule.join(userDataPath, "/dict"),
+        languages: pathModule.join(userDataPath, "/lang"),
         schema: pathModule.join(userDataPath, "/schema.json"),
         templatesDir: pathModule.join(userDataPath, "/templates"),
         tmp: tmpPath,
