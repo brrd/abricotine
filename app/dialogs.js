17a46fe7053bbf8e3a8401dee2c907b21997039e
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2016-07-06 11:58:00 +0200

    I18n of dialog boxes

--- app/dialogs.js
+++ app/dialogs.js
@@ -1,8 +1,8 @@
 /*
-*   Abricotine - Markdown Editor
-*   Copyright (c) 2015 Thomas Brouard
-*   Licensed under GNU-GPLv3 <http://www.gnu.org/licenses/gpl.html>
-*/
+ *   Abricotine - Markdown Editor
+ *   Copyright (c) 2015 Thomas Brouard
+ *   Licensed under GNU-GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ */
 
 var BrowserWindow = require("browser-window"),
     constants = require("./constants.js"),
@@ -13,7 +13,7 @@ var BrowserWindow = require("browser-window"),
     parsePath = require("parse-filepath");
 
 // Returns the most "logical" window object (it is quite useless actually)
-function getWindow (win) {
+function getWindow(win) {
     if (typeof win === "number") {
         return BrowserWindow.fromId(win);
     } else if (win instanceof BrowserWindow) {
@@ -35,7 +35,7 @@ var appDialogs = {
             options = {
                 title: appDialogs.localizer.get("about-title", "About"),
                 message: appDialogs.localizer.get("about-message", "ABRICOTINE - MARKDOWN EDITOR (v. %0)\n\nCopyright (c) 2015 Thomas Brouard\n\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.", [constants.appVersion]),
-                buttons: ["OK"],
+                buttons: [appDialogs.localizer.get('button-ok', 'OK')],
                 icon: image
             };
         if (win) {
@@ -54,7 +54,7 @@ var appDialogs = {
             userChoice = dialog.showMessageBox(win, {
                 title: appDialogs.localizer.get('confirm-close-title', 'Unsaved document'),
                 message: appDialogs.localizer.get('confirm-close-message', "Do you really want to close '%0' without saving?", [filename]),
-                buttons: ['Cancel', appDialogs.localizer.get('confirm-save-and-close', 'Save & close'), appDialogs.localizer.get('confirm-close-without-saving', 'Close without saving')]
+                buttons: [appDialogs.localizer.get('button-cancel', 'Cancel'), appDialogs.localizer.get('confirm-save-and-close', 'Save & close'), appDialogs.localizer.get('confirm-close-without-saving', 'Close without saving')]
             });
         switch (userChoice) {
             case 1:
@@ -93,12 +93,14 @@ var appDialogs = {
     },
 
     askOpenImage: function (title, win) {
-        //TODO i18n
         win = getWindow(win);
         var path = dialog.showOpenDialog(win, {
-            title: title || 'Insert image',
+            title: title || appDialogs.localizer.get('insert-image', 'Insert image'),
             properties: ['openFile'],
-            filters: [{ name: 'Images', extensions: ['jpg', 'jpeg', 'png', 'gif', 'svg'] }],
+            filters: [{
+                name: appDialogs.localizer.get('insert-image-filter', 'Images'),
+                extensions: ['jpg', 'jpeg', 'png', 'gif', 'svg']
+            }],
             defaultPath: process.cwd()
         });
         if (path) {
@@ -108,13 +110,12 @@ var appDialogs = {
     },
 
     askNeedSave: function (abrDoc, callback, win) {
-        //TODO i18n
         win = getWindow(win);
         var userChoice = dialog.showMessageBox(win, {
-                title: 'Save document',
-                message: 'The current document needs to be saved before performing this operation.',
-                buttons: ['Cancel', 'Save document']
-            });
+            title: appDialogs.localizer.get('dialog-save', 'Save document'),
+            message: appDialogs.localizer.get('dialog-save-message', 'The current document needs to be saved before performing this operation.'),
+            buttons: [appDialogs.localizer.get('button-cancel', 'Cancel'), appDialogs.localizer.get('dialog-save', 'Save document')]
+        });
         if (userChoice === 1) {
             abrDoc.save(null, callback);
         }
@@ -122,12 +123,11 @@ var appDialogs = {
     },
 
     fileAccessDenied: function (path, callback, win) {
-        //TODO i18n
         win = getWindow(win);
         var userChoice = dialog.showMessageBox(win, {
-            title: "Permission denied",
-            message: "The file '" + path + "' could not be written: permission denied. Please choose another path.",
-            buttons: ['Cancel', 'OK']
+            title: appDialogs.localizer.get("permission-denied", "Permission denied"),
+            message: appDialogs.localizer.get("permission-denied-message", "The file '%0' could not be written: permission denied. Please choose another path.", path),
+            buttons: [appDialogs.localizer.get('button-cancel', 'Cancel'), appDialogs.localizer.get('button-ok', 'OK')]
         });
         if (userChoice === 1) {
             callback();
@@ -136,12 +136,11 @@ var appDialogs = {
     },
 
     importImagesDone: function (path, win) {
-        //TODO i18n
         win = getWindow(win);
         dialog.showMessageBox(win, {
-            title: "Images copied",
-            message: "Document images have been copied in the '" + path + "' directory.",
-            buttons: ['OK']
+            title: appDialogs.localizer.get("images-copied", "Images copied"),
+            message: appDialogs.localizer.get("images-copied-message", "Document images have been copied in the '%0' directory.", path),
+            buttons: [appDialogs.localizer.get('button-ok', 'OK')]
         });
     }
 };
