b0b28193ded09b3de45680a05ba0d75729191171
brrd <contact@brrd.fr>
2017-03-06 17:59:24 +0100

    Localize: load and merge  multiple lang files

--- app/localize.js
+++ app/localize.js
@@ -6,39 +6,43 @@
 
 var app = require("electron").app,
     constants = require("./constants.js"),
-    files = require.main.require("./files.js"),
+    deepExtend = require("deep-extend"),
     paths = require("path"),
     fs = require('fs');
 
-function Localizer(language) {
-    this.translations = {};
-    language = language || app.getLocale();
-    this.language = language;
-
-    var languageFile = paths.join(constants.path.languages, language + ".json"),
-        fileContent;
-
-    if (files.fileExists(languageFile)) {
-        // TODO find approaching locale when exact language file does not exist (e.g. load "fr.json" when "fr_BE.json" is asked)
+function getTranslations(lang) {
+    var langFiles = [
+            paths.join(constants.path.defaultDir, "lang", "en.json"),
+            paths.join(constants.path.defaultDir, "lang", lang + ".json"),
+            paths.join(constants.path.userData, "lang", lang + ".json")
+        ],
+        translations = [];
+    langFiles.forEach(function(file) {
         try {
-            fileContent = fs.readFileSync(languageFile, 'utf8');
-            this.translations = JSON.parse(fileContent);
+            var fileContent = fs.readFileSync(file, 'utf8');
+            translations.push(JSON.parse(fileContent));
         } catch (err) {
-            console.warn("Error while loading localization data (from file '" + languageFile + "'): " + err);
+            console.warn("Error while loading localization data: " + err);
         }
-    } else {
-        console.warn("Error while loading localization data (from file '" + languageFile + "'): file does not exist");
-    }
+    });
+    return deepExtend.apply(this, translations);
+}
+
+function Localizer(lang) {
+    this.translations = {};
+    lang = lang || app.getLocale();
+    this.lang = lang;
+    this.translations = getTranslations(lang);
 }
 
 Localizer.prototype = {
 
-    getLanguage: function () {
-        return this.language;
+    getLang: function () {
+        return this.lang;
     },
 
     get: function (key, defaultTranslation, argsArray) {
-        var translation = null;
+        var translation;
         if (this.translations[key]) {
             translation = this.translations[key];
         } else if (defaultTranslation) {
