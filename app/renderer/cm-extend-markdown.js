fad6b0c158813173187cf6b412b7097e320a86a7
Thomas Brouard <contact@brrd.fr>
2017-10-02 12:46:16 +0200

    Add header text

--- app/renderer/cm-extend-markdown.js
+++ app/renderer/cm-extend-markdown.js
@@ -1,3 +1,9 @@
+/*
+*   Abricotine - Markdown Editor
+*   Copyright (c) 2015 Thomas Brouard
+*   Licensed under GNU-GPLv3 <http://www.gnu.org/licenses/gpl.html>
+*/
+
 /* jshint esversion: 6 */
 // TODO: support multiple selections
 // TODO: reverse images and links
